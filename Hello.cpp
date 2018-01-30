7641e09e2c9578d490088b4a34ecfa6b8611aa72
Britani Prescott <britanierin@me.com>
2018-01-29 18:59:03 -0600

    Add function 'multiply'

--- Hello.cpp
+++ Hello.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-int add(int x, int y);
+int multiply(int x, int y);
 int main()
 {
 	std::cout<<"Hello World!\n";
 	
 	return 0;
 }
-int add(int x, int y)
+int multiply(int x, int y)
 {
-	return x + y;
+	return x * y;
 }
